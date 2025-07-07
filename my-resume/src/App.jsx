814ab243e80bcd6f6200e11230ba77d5522961e4
alliananaz <alliana.nazareno@gmail.com>
2025-07-07 21:39:19 +0200

    change layout, using grid. (with old code just in case)

--- my-resume/src/App.jsx
+++ my-resume/src/App.jsx
@@ -6,20 +6,7 @@ export default function App() {
   return (
     <>
       <Header/>
-      <div className='resume space-y-6 mt-6'>
-        <h1 className="font-bold text-red-500"> Period💅</h1>
-        
-        <h1 className="text-red-500 underline">Hello Tailwind</h1>
-      </div> 
-      {/* <hr className="my-3 border-t border-gray-400" />  border line */}
-      <div className="flex flex-col w-full h-full mt-6">
-          <div className="flex flex-row">
-              <div className="flex-1 flex border justify-center items-center">A</div>
-              <div className="flex-1 flex border justify-center items-center">B</div>
-              <div className="flex-1 flex border justify-center items-center">C</div>
-          </div>
-          <div className="flex border justify-center items-center">D</div>
-      </div>
+      
     </>
   )
 }
--- my-resume/src/Header.jsx
+++ my-resume/src/Header.jsx
@@ -1,10 +1,28 @@
 export default function Header(){
     return (
-        <header className="header">
-            <div className="hdr">
-                <h1> Project Portfolio </h1>
-                <h2>Alliana Nazareno</h2>
-                <h3>alliana.nazareno@gmail.com</h3> {/* gjør om til en clickable link senere? */}
+        <header className="w-full bg-yellow-200 p-6 mt-6 items-center">
+            <div className="hdr w-full space-y-6 items-center">
+                <div className="grid grid-row-5 gap-2 items-center">
+                    <div className="text-3xl font-semibold">Alliana Shari Nazareno</div>
+                    <div className="mt-4"> 14. mai 2004 </div>
+                    <div> +47 465 37 422 </div>
+                    <div> alliana.nazareno@gmail.com </div>
+                    <div> LinkedIn </div>
+                </div>
+                {/* <div className="flex flex-col mt-3">
+                    <div className="text-3xl flex-1 flex justify-center items-center">Alliana Nazareno</div>
+                    <div className="flex flex-row">
+                        
+                        <div className="flex-1 flex justify-center items-center">
+                            <h3>14. mai 2003 </h3>
+                        </div>
+                        <div className="flex-1 flex justify-start items-center">alliana.nazareno@gmail.com <b/></div>
+                        <div className="flex-1 flex justify-center items-center">+0000000</div>
+                        <div className="flex-1 flex justify-center items-center">
+                            <p><a href="www.linkedin.com/in/alliana-shari-nazareno-765276329">LinkedIn</a></p>
+                        </div>
+                    </div>
+                </div> */}
                 <hr></hr>
             </div>
         </header>
