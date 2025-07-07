a1cb782fc93844698b42248f14612b60cd6f79bd
alliananaz <alliana.nazareno@gmail.com>
2025-07-07 16:54:44 +0200

    work FINALLYYY. testing of tailwindcss egenskaper. for doc porp only.

--- my-resume/src/App.jsx
+++ my-resume/src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import Header from './Header.jsx'
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    // <h1 className="text-9xl font-bold underline text-red-500">
-    //   Period💅
-    // </h1>
-    // <div className="flex flex-col w-full h-full">
-    //     <div className="flex flex-row">
-    //         <div className="flex-1 flex border justify-center items-center">A</div>
-    //         <div className="flex-1 flex border justify-center items-center">B</div>
-    //         <div className="flex-1 flex border justify-center items-center">C</div>
-    //     </div>
-    //     <div className="flex border justify-center items-center">D</div>
-    // </div>
-    <h1 className="text-red-500 text-4xl">Hello Tailwind</h1>
-
+    <>
+      <Header/>
+      <div className='resume space-y-6 mt-6'>
+        <h1 className="font-bold text-red-500"> Period💅</h1>
+        
+        <h1 className="text-red-500 underline">Hello Tailwind</h1>
+      </div> 
+      {/* <hr className="my-3 border-t border-gray-400" />  border line */}
+      <div className="flex flex-col w-full h-full mt-6">
+          <div className="flex flex-row">
+              <div className="flex-1 flex border justify-center items-center">A</div>
+              <div className="flex-1 flex border justify-center items-center">B</div>
+              <div className="flex-1 flex border justify-center items-center">C</div>
+          </div>
+          <div className="flex border justify-center items-center">D</div>
+      </div>
+    </>
   )
 }
---
+++ my-resume/src/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return (
+        <header className="header">
+            <div className="hdr">
+                <h1> Project Portfolio </h1>
+                <h2>Alliana Nazareno</h2>
+                <h3>alliana.nazareno@gmail.com</h3> {/* gjør om til en clickable link senere? */}
+                <hr></hr>
+            </div>
+        </header>
+    )
+}
